344e6d011ebd7f3de2f9ad3e46265974cc5a170b
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-09-17 12:49:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  if (decimal === 0) {
+        return "0"; // Special case for 0
+    }
+    
+    return decimal.toString(2);
 }
 
 window.decimalToBinary = decimalToBinary;
